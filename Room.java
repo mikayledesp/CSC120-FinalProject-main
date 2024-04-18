10f95006fb2b42642d8d9dac7f33cbb631ef999a
mikayledesp <mdespaigne@smith.edu>
2024-04-18 15:44:51 -0400

    made the rooms instances instead of classes

--- Room.java
+++ Room.java
@@ -43,14 +43,23 @@ public class Room {
 // examine method
 
 
-//fight method 
+//fight method
+// print out statement "You are now in fight with "  + opponent 
+// lists 4 possible options 
+// if statement that correlates the right option with defeating the opponent ie(right punch destroys bear put not a sculpture or snake )
+// if wring one is picked you loose. 
 
 // enterRoom method 
  //if room choice is more than 1, make print statement asking user what room they want to go in
 
 
+ // show options method 
+ // has list of possible actions/ keywords
+ // would override in rooms that have other action options.
 
-// Look around method shows artifacts 
+
+
+// Look around method shows room name and description(includes artifacts)
 
  public String toString() {
    return name + " (" + desc + ")";
@@ -59,36 +68,15 @@ public class Room {
  public static void main(String[] args) {
 
    
-
-   // Example ImmutableGraph with Buildings as Node data
-  
- Room portrait1 = new Room("Portrait Gallery", "Add later", 1);
- Room apothacary2 = new Room("Old Apothacary", "Add Later", 2);
- Room historic3 = new Room("Historical House", "Add Later", 2);
- Room sculpture4 = new Room("Sculpture Garden", "Add Later", 2);
- Room armor5 = new Room("Armor and Weapons Gallery", "Add Later", 3);
- Room Animal6 = new Room("Animal Exhibit", "Add Later", 1);
-// portrait = 
-
-
+  Room room1 = new Room("Portrait Gallery", null, 0);
+  Room room2 = new Room("Apothacary", null, 0);
+  Room room3 = new Room("Historic", null, 0);
+  Room room4 = new Room("Sculpture", null, 0);
+  Room room5 = new Room("Armory", null, 0);
+  Room room6 = new Room("Animal", null, 0);
  
-   ImmutableGraph<Room> myGraph1 = GraphBuilder.undirected()
-     .<Room>immutable()
-     .putEdge(portrait1, apothacary2)
-     .putEdge(apothacary2, historic3)
-     .putEdge(apothacary2, sculpture4)
-     .putEdge(historic3,sculpture4)
-     .putEdge(sculpture4, armor5)
-     .putEdge(armor5, Animal6)
-     
 
-   //   .putEdge(mcConnell, sabinReed)
-   //   .putEdge(sabinReed, burton)
-     .build();
-
-    //  System.out.println(myGraph1);
- }
-
+}
 }
 
 
--- museum.java
+++ museum.java
@@ -1,10 +1,56 @@
 import java.util.Scanner;
+import com.google.common.graph.*;
 
 public class museum {
+   
+    Graph <Room> map;
+    Room currentLoc;
+    public museum(){
+        Room portrait1 = new Room("Portrait Gallery", "Add later", 1);
+        Room apothacary2 = new Room("Old Apothacary", "Add Later", 2);
+        Room historic3 = new Room("Historical House", "Add Later", 2);
+        Room sculpture4 = new Room("Sculpture Garden", "Add Later", 2);
+        Room armor5 = new Room("Armor and Weapons Gallery", "Add Later", 3);
+        Room Animal6 = new Room("Animal Exhibit", "Add Later", 1);
+        // portrait = 
+
+
+    
+    ImmutableGraph<Room> myGraph1 = GraphBuilder.undirected()
+        .<Room>immutable()
+        .putEdge(portrait1, apothacary2)
+        .putEdge(apothacary2, historic3)
+        .putEdge(apothacary2, sculpture4)
+        .putEdge(historic3,sculpture4)
+        .putEdge(sculpture4, armor5)
+        .putEdge(armor5, Animal6)
+        
+
+    //   .putEdge(mcConnell, sabinReed)
+    //   .putEdge(sabinReed, burton)
+        .build();
+
+        //  System.out.println(myGraph1);
+        // this.map = GraphBuilder< >.build();
+        this.currentLoc = portrait1;
+        }
+        
+
+
+
     public static void main(String[] args) {
+        Room room1 = new Room("Portrait Gallery", null, 0);
+        Room room2 = new Room("Apothacary", null, 0);
+        Room room3 = new Room("Historic", null, 0);
+        Room room4 = new Room("Sculpture", null, 0);
+        Room room5 = new Room("Armory", null, 0);
+        Room room6 = new Room("Animal", null, 0);
 
         // This is a "flag" to let us know when the loop should end
         boolean stillPlaying = true;
+        museum mymuseum = new museum();
+         mymuseum.currentLoc();
+        
 
         // We'll use this to get input from the user.
         Scanner userInput = new Scanner(System.in);
@@ -13,19 +59,19 @@ public class museum {
         String userResponse = "";
 
         // This could be replaced with a more interesting opening
-        System.out.println("******************");
-        System.out.println("WELCOME TO MY GAME");
-        System.out.println("******************");
+        System.out.println("˙✧˖°🏛 ༘ ⋆｡°˙✧˖°🏛 ༘ ⋆｡°˙✧˖°🏛 ༘ ⋆｡°");
+        System.out.println("    Welcome To Our Game! ");
+        System.out.println("˙✧˖°🏛 ༘ ⋆｡°˙✧˖°🏛 ༘ ⋆｡°˙✧˖°🏛 ༘ ⋆｡°");
 
         // Instructions are sometimes helpful
-        System.out.println("Enter WIN to win, LOSE to lose, anything else to continue playing.");
+        System.out.println("Game Description w insctuctions ");
 
         // The do...while structure means we execute the body of the loop once before checking the stopping condition
         do {
             // ************************************************
             // The stuff that happens in your game will go here
             //  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓
-            System.out.println("You are still playing. Follow the instructions if you want to win/lose...");
+             System.out.println("You have now entered the  " + this.room1 + "");
             userResponse = userInput.nextLine().toUpperCase();
 
             // ***********************************************************************
