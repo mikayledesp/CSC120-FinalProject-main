db2676f80ae87f2001e8f065e982b6ef73259701
mikayledesp <mdespaigne@smith.edu>
2024-05-01 23:20:03 -0400

    added some java doc

--- DisplayImage.java
+++ DisplayImage.java
@@ -3,7 +3,8 @@ import javax.swing.*;
 
     public class DisplayImage extends Canvas {
     /**
-     * 
+     * Method sets up the map image so that it can be printed in the game loop 
+     * @param Graphics 
      * 
      */
     public void paint(Graphics g) {
